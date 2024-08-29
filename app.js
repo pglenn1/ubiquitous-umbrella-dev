676985f7d8b4da755f74fd8e511e1fbab65e1b8b
Alex Glenn <157517894+pglenn1@users.noreply.github.com>
2024-08-29 02:34:43 +0000

    commit

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files from the current directory
+app.use(express.static(path.join(__dirname)));
+
+// Serve the index.html file
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 console.log('im on a node server, yo');
 
-app.get('/', function (req, res) {
-  res.send('Hello Node from Ex on local dev box')
-})
-
-app.listen(5000)
+const PORT = 5000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
