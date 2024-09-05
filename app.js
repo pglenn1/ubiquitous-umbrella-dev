bf7e65b15c5d4192098e52f56d650cec3c9886db
Glenn <pglenn1@una.edu>
2024-09-05 13:18:11 -0500

    mangoDB

--- app.js
+++ app.js
@@ -1,18 +1,51 @@
-const express = require('express');
-const path = require('path');
-const app = express();
+const express = require('express')
+const app = express()
+app.set('view engine', 'ejs')
+app.use(express.static('./public/'))
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.uri; 
 
-// Serve static files from the current directory
-app.use(express.static(path.join(__dirname)));
+console.log(uri);
 
-// Serve the index.html file
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+// console.log('im on a node server change that and that tanad f, yo');
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-console.log('im on a node server, yo');
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
-const PORT = 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+app.get('/', function (req, res) {
+  // res.send('Hello Node from Ex on local dev box')
+  res.sendFile('index.html');
+})
+
+app.get('/ejs', (req,res)=>{
+``
+  res.render('index', {
+    myServerVariable : "something from server"
+  });
+
+  //can you get content from client...to console? 
+})
+
+app.listen(5000)
